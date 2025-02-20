20995e76d37a8f4fd9c115bf03780da2c23b2c2c
Khawla <khawlakadry@gmail.com>
2025-02-20 15:11:05 +0400

    🔨 update diaRecipient address

--- contracts/scripts/sendMessage.mjs
+++ contracts/scripts/sendMessage.mjs
@@ -10,14 +10,21 @@ const { ethers } = pkg;
 
 async function main() {
   let destinationDomain = 10640;
-  let diaRecipient = "0x97C989740aE765518FA85E64ED61512D39765e43"
-  let formattedRecipient = addressToBytes32(
-    diaRecipient
-  );
+  let diaRecipient;
+  let formattedRecipient;
 
   const network = await ethers.provider.getNetwork();
   console.log("Chain ID:", network.chainId);
 
+  if(network.chainId === 11155420n){
+    diaRecipient = "0x5f3Ae50D513fA347fB39571fF81DFa56AAf385D6";
+  } else {
+    diaRecipient = "0x97C989740aE765518FA85E64ED61512D39765e43";
+  }
+
+  formattedRecipient = addressToBytes32(
+    diaRecipient
+  );
 
   const key =  "ETH/USD"; // Assuming key is an address or a bytes32 value
  
@@ -36,38 +43,33 @@ async function main() {
   let oracleRequestorAddress;
 
   switch (network.chainId){
+    // Base Sepolia
     case 84532n: {
        mailBoxAddress = "0x6966b0E55883d49BFB24539356a2f8A673E02039"
       oracleRequestorAddress = "0x9D13514FA039f21EB39BC746cFAE700404AdD97F"
     }
     break;
 
+    // Ethereum Sepolia
     case 11155111n: {
       mailBoxAddress = "0xfFAEF09B3cd11D9b20d1a19bECca54EEC2884766"
       oracleRequestorAddress = "0x3b64691c14bca163c8230e726c6f880b0e74ab0d"
     }
     break;
 
-
+    // Arbitrum Sepolia
     case 421614n: {
        mailBoxAddress = "0x598facE78a4302f11E3de0bee1894Da0b2Cb71F8"  
       oracleRequestorAddress = "0xb31034945a91E6d10afEf68101a0F50A069842c9"
     }
     break;
+
+    // OP Sepolia
     case 11155420n: {
       mailBoxAddress = "0x6966b0E55883d49BFB24539356a2f8A673E02039"
-     oracleRequestorAddress = "0xbEc3e192175D1bEEdb7ACc4b87F7d158fF363841"
-   }
-   
-
-    
+     oracleRequestorAddress = "0xbb3673d4c8c0B6681E59BAD091d506567812f790"
+   } 
     break;
-    default:{
- 
-      mailBoxAddress = "0x6966b0E55883d49BFB24539356a2f8A673E02039"
-      oracleRequestorAddress = "0x52A2F754d876bF15aE61cfAC62c7d948699965D9"
-    }
-
   }
 
   const mailbox = await ethers.getContractAt(
@@ -110,8 +112,6 @@ async function main() {
     console.log("err",e);
 
   }
-
- 
 
 }
 
